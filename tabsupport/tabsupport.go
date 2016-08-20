640b339f564e0a42a6355007e7af58aaa854e13d
Dmitri Shuralyov <shurcooL@gmail.com>
2016-08-19 22:19:14 -0700

    tabsupport: Add KeyDownHandler event handler.
    Unlike the Add helper, this version is easier to embed directly in HTML.
    
    This API is still experimental and may be removed if unused.

--- tabsupport/tabsupport.go
+++ tabsupport/tabsupport.go
@@ -25,3 +25,33 @@ func Add(textArea *dom.HTMLTextAreaElement) {
 		}
 	})
 }
+
+// KeyDownHandler is a keydown event handler for a <textarea> element.
+// It makes it so that pressing tab key will insert tabs.
+//
+// To use it, first make it available to the JavaScript world, e.g.:
+//
+// 	js.Global.Set("TabSupportKeyDownHandler", jsutil.Wrap(tabsupport.KeyDownHandler))
+//
+// Then use it as follows in the HTML:
+//
+// 	<textarea onkeydown="TabSupportKeyDownHandler(this, event);"></textarea>
+//
+func KeyDownHandler(element dom.HTMLElement, event dom.Event) {
+	switch ke := event.(*dom.KeyboardEvent); {
+	case ke.KeyCode == '\t' && !ke.CtrlKey && !ke.AltKey && !ke.MetaKey && !ke.ShiftKey: // Tab.
+		textArea := element.(*dom.HTMLTextAreaElement)
+
+		value, start, end := textArea.Value, textArea.SelectionStart, textArea.SelectionEnd
+
+		textArea.Value = value[:start] + "\t" + value[end:]
+
+		textArea.SelectionStart, textArea.SelectionEnd = start+1, start+1
+
+		event.PreventDefault()
+
+		// Trigger "input" event listeners.
+		inputEvent := js.Global.Get("CustomEvent").New("input")
+		textArea.Underlying().Call("dispatchEvent", inputEvent)
+	}
+}
