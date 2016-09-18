181822dd2b2187367f11cbcaf16c261fc16f4849
Dmitri Shuralyov <shurcooL@gmail.com>
2016-09-17 21:30:29 -0700

    table-of-contents/handler: Use keyed fields in composite literal.
    Fixes go vet issue.

--- table-of-contents/handler/main.go
+++ table-of-contents/handler/main.go
@@ -13,8 +13,8 @@ import (
 
 func init() {
 	// HACK: This code registers routes at root on default mux... That's not very nice.
-	http.Handle("/table-of-contents.js", httputil.FileHandler{gopherjs_http.Package("github.com/shurcooL/frontend/table-of-contents")})
-	http.Handle("/table-of-contents.css", httputil.FileHandler{vfsutil.File(filepath.Join(importPathToDir("github.com/shurcooL/frontend/table-of-contents"), "style.css"))})
+	http.Handle("/table-of-contents.js", httputil.FileHandler{File: gopherjs_http.Package("github.com/shurcooL/frontend/table-of-contents")})
+	http.Handle("/table-of-contents.css", httputil.FileHandler{File: vfsutil.File(filepath.Join(importPathToDir("github.com/shurcooL/frontend/table-of-contents"), "style.css"))})
 }
 
 func importPathToDir(importPath string) string {
